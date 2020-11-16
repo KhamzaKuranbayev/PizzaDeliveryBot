35ca423fb07bf122a23d53d08bf81612b07a3c1b
Kamol <kamolnarkulov2003@gmail.coma>
2020-11-16 14:13:11 +0500

    Added startText() and setLangButtons() methods

--- src/main/java/pizza_user/UserBot.java
+++ src/main/java/pizza_user/UserBot.java
@@ -2,8 +2,14 @@ package pizza_user;
 
 import models.Manager;
 import models.User;
+import org.checkerframework.checker.units.qual.K;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import pizza_manager.ManagerBot;
 
 import java.util.ArrayList;
@@ -21,9 +27,33 @@ public class UserBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
-
     }
+
+    public void startText(SendMessage sendMessage) {
+        sendMessage.setText(""); // ==> !
+        try {
+            setLangButtons(sendMessage);
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setLangButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardButton buttonUz = new KeyboardButton("\uD83C\uDDFA\uD83C\uDDFFUzbek");
+        KeyboardButton buttonRu = new KeyboardButton("\uD83C\uDDF7\uD83C\uDDFAРусский");
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRow.add(buttonUz);
+        keyboardRow.add(buttonRu);
+        keyboardRows.add(keyboardRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+    }
+
 
     // Kamol
     // 1ta text va 2ta button qo'shish kerak uzb, rus
@@ -41,16 +71,16 @@ public class UserBot extends TelegramLongPollingBot {
         return false;
 
     }
-        // Dior
-        // yangi userni users dan check qiladigan method yozish, boolean
+    // Dior
+    // yangi userni users dan check qiladigan method yozish, boolean
 
-        @Override
-        public String getBotUsername () {
-            return "http://t.me/uzbek_pizza_uzbot";
-        }
-
-        @Override
-        public String getBotToken () {
-            return TOKEN;
-        }
+    @Override
+    public String getBotUsername() {
+        return "http://t.me/uzbek_pizza_uzbot";
     }
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+}
